41a95e18dedbd79947fc8d38f9bf99d4e39f7499
luizcarlos1979 <luizcarlos1979@gmail.com>
2026-02-03 01:08:22 +0000

    Servidor criado com / e /oi

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ app.get('/', (req, res) => {
   res.send('Hello World - Luiz Carlos Aires de Macêdo')
 
 })
+app.get('/oi', (req, res) => {
+
+  res.send('\olá Mundo !')
+
+})
 app.listen(3000, () => {
 
   console.log('Server is running on http://localhost:3000')
